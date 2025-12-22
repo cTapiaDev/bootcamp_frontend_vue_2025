614d675b554e5c07df1502a8260dbd2efa6febbd
Carlos Tapia <ctapiadev@gmail.com>
2025-12-22 12:22:33 -0300

    flex

--- modulo3/3-bootstrap/js/app.js
+++ modulo3/3-bootstrap/js/app.js
@@ -11,8 +11,10 @@ const navbar = document.querySelector('#mainNav');
 const handleScroll = () => {
     if (window.scrollY > 50) {
         navbar.classList.add('navbar-scrolled');
+        navbar.classList.remove('bg-dark');
     } else {
         navbar.classList.remove('navbar-scrolled');
+        navbar.classList.add('bg-dark');
     }
 }
 
